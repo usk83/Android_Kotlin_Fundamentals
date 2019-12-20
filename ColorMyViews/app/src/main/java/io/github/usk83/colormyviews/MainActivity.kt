5cf46b59c3acf1f3fc5b4cd3b13d80e0fabe7b4f
Yusuke Takahashi <usk@sfc.wide.ad.jp>
2019-12-19 16:06:14 -0800

    Work in Progress

--- ColorMyViews/app/src/main/java/io/github/usk83/colormyviews/MainActivity.kt
+++ ColorMyViews/app/src/main/java/io/github/usk83/colormyviews/MainActivity.kt
@@ -1,12 +1,63 @@
 package io.github.usk83.colormyviews
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
     }
+
+    private fun makeColored(view: View) {
+        when (view.id) {
+            // Boxes using Color class colors for the background
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
+    }
+
+    private fun setImage(view: View) {
+        when (view.id) {
+            R.id.box_one_text -> view.setBackgroundResource(R.drawable.image_one)
+            R.id.box_two_text -> view.setBackgroundResource(R.drawable.image_two)
+            R.id.box_three_text -> view.setBackgroundResource(R.drawable.image_three)
+            R.id.box_four_text -> view.setBackgroundResource(R.drawable.image_four)
+            R.id.box_five_text -> view.setBackgroundResource(R.drawable.image_five)
+            else -> view.setBackgroundResource(R.drawable.image_bg)
+        }
+    }
+
+    private fun setListeners() {
+        val boxOneText = findViewById<TextView>(R.id.box_one_text)
+        val boxTwoText = findViewById<TextView>(R.id.box_two_text)
+        val boxThreeText = findViewById<TextView>(R.id.box_three_text)
+        val boxFourText = findViewById<TextView>(R.id.box_four_text)
+        val boxFiveText = findViewById<TextView>(R.id.box_five_text)
+
+        val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
+
+        val clickableViews: List<View> =
+            listOf(
+                boxOneText, boxTwoText, boxThreeText,
+                boxFourText, boxFiveText, rootConstraintLayout
+            )
+
+        for (item in clickableViews) {
+            item.setOnClickListener {
+                // makeColored(it)
+                setImage(it)
+            }
+        }
+    }
+
 }
